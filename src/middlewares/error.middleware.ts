58750586bee0648fa2c4d8087fd4879bcdfc9240
sidratul <muhammad.mochi@gmail.com>
2024-12-15 03:21:23 +0700

    fix error handling

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,10 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 export async function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  if (res.headersSent) {
-    return next(err);
-  }
-
   res.status(500)
-  res.render('error', { error: err })
+  res.json({message: (err as Error).message})
 }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -2,18 +2,30 @@ import { Router } from 'express';
 import * as userService from './user.service';
 import { validate } from '../middlewares/validation.middleware';
 import { UserUpdateData, userUpdateValidation } from './user.validation';
+import { errorHandler } from '../middlewares/error.middleware';
 
 export const userRouter = Router();
 
-userRouter.get('/fetch-user-data', async (req, res) => {
-  const authUser = res.locals.user;
-  const user = await userService.findOrCreateUser(authUser.id, authUser.email);
-  res.json(user);
+userRouter.get('/fetch-user-data', async (req, res, next) => {
+  try{
+    const authUser = res.locals.user;
+    const user = await userService.findOrCreateUser(authUser.id, authUser.email);
+    res.json(user);
+  } catch(err) {
+    next(err);
+  }
 });
 
-userRouter.patch('/update-user-data', validate(userUpdateValidation), async (req, res) => {
-  const data = req.body as UserUpdateData;
-  const authUser = res.locals.user;
-  const user = await userService.updateUser(authUser.id,  data);
-  res.json(user)
+userRouter.patch('/update-user-data', validate(userUpdateValidation), async (req, res, next) => {
+  try{
+    const data = req.body as UserUpdateData;
+    const authUser = res.locals.user;
+    const user = await userService.updateUser(authUser.id,  data);
+    res.json(user)
+  } catch(err) {
+    next(err);
+  }
+  
 });
+
+userRouter.use(errorHandler);
